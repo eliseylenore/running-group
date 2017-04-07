996beb4bead8c383b443e3ba029365f423958ec1
Elise St Hilaire <eliseylenore@gmail.com>
2017-04-07 11:29:56 -0700

    Create edit form

--- src/app/edit-runner/edit-runner.component.ts
+++ src/app/edit-runner/edit-runner.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Runner } from '../runner.model';
 
 @Component({
   selector: 'app-edit-runner',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-runner.component.css']
 })
 export class EditRunnerComponent implements OnInit {
+  @Input() selectedRunner;
 
   constructor() { }
 
