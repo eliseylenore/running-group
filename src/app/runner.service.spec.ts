65478b9fecdeac8b409b7f8e67b3c0c82400189f
Elise St Hilaire <eliseylenore@gmail.com>
2017-04-07 09:13:01 -0700

    Add RunnerService

---
+++ src/app/runner.service.spec.ts
@@ -0,0 +1,16 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async, inject } from '@angular/core/testing';
+import { RunnerService } from './runner.service';
+
+describe('RunnerService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [RunnerService]
+    });
+  });
+
+  it('should ...', inject([RunnerService], (service: RunnerService) => {
+    expect(service).toBeTruthy();
+  }));
+});
--- src/app/runners-list/runners-list.component.ts
+++ src/app/runners-list/runners-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { RunnerService } from '../runner.service';
 
 @Component({
   selector: 'app-runners-list',
   templateUrl: './runners-list.component.html',
-  styleUrls: ['./runners-list.component.css']
+  styleUrls: ['./runners-list.component.css'],
+  providers: [RunnerService]
 })
 export class RunnersListComponent implements OnInit {
+  runners: Runner[];
 
-  constructor() { }
+  constructor(private runnerService: RunnerService) { }
 
   ngOnInit() {
+    this.runners = this.runnerServie.getRunners();
   }
 
 }
