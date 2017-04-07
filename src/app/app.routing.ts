c4193210209f4b68624102f9453dea3d72e401ed
Elise St Hilaire <eliseylenore@gmail.com>
2017-04-07 10:06:16 -0700

    runner-detail route works when click on runner in list

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,15 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { RunnerDetailsComponent } from './runner-details/runner-details.component';
 
 const appRoutes: Routes = [
   {
     path: '',
     component: WelcomeComponent
+  }, {
+    path: 'runner-details',
+    component: RunnerDetailsComponent
   }
 ];
 
