f418361e11b0e51b6e435d4fbdb64254a4d1e26a
Elise St Hilaire <eliseylenore@gmail.com>
2017-04-07 10:41:23 -0700

    Create route for admin

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { RunnerDetailsComponent } from './runner-details/runner-details.component';
+import { AdminComponent } from './admin/admin.component';
 
 const appRoutes: Routes = [
   {
@@ -10,6 +11,9 @@ const appRoutes: Routes = [
   }, {
     path: 'runners/:id',
     component: RunnerDetailsComponent
+  }, {
+    path: 'admin',
+    component: AdminComponent
   }
 ];
 
