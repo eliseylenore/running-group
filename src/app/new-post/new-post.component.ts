19b2ab159700191aa110778470c38031862af523
Elise St Hilaire <eliseylenore@gmail.com>
2017-04-07 16:30:17 -0700

    Ability to add new chats

--- src/app/new-post/new-post.component.ts
+++ src/app/new-post/new-post.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../post.model';
+import { PostService } from '../post.service';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
   styleUrls: ['./new-post.component.css']
 })
-export class NewPostComponent implements OnInit {
+export class NewPostComponent{
+  newPost: Post;
+  constructor(private postService: PostService, private router: Router) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  submitForm(author: string, text: string) {
+    this.newPost = new Post(author, text);
+    this.postService.addPost(this.newPost);
   }
 
 }
--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -14,4 +14,8 @@ export class PostService {
     return this.posts;
   }
 
+  addPost(postToBeSaved) {
+    this.posts.push(postToBeSaved);
+  }
+
 }
